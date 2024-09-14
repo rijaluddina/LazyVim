22231e271c521a01315340d23bc8eb9576576724
rijaluddina <rijaluddin.alazmi@outlook.com>
2024-09-15 05:56:49 +0700

    fix :TSInstall blade

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -258,43 +258,13 @@ if true then return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
 
   -- add more treesitter parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "blade",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "php",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    },
-  },
-
   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
   -- If you'd rather extend the default config, use the code below instead:
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
-        "blade",
-        "php",
-      })
+      vim.list_extend(opts.ensure_installed, {})
     end,
 
     config = function(_, opts)
@@ -312,7 +282,7 @@ if true then return {
           files = { "src/parser.c" },
           branch = "main",
         },
-        filetype = "blade",
+        filetype = "blade", -- after this run :TSInstall blade
       }
     end,
   },
@@ -328,7 +298,6 @@ if true then return {
         "stylua",
         "shellcheck",
         "shfmt",
-        "flake8",
       },
     },
   },
