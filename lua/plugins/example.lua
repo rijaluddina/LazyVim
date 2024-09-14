87f652289de64c5e03c3f9844f4ccd5f1836beb4
rijaluddina <rijaluddin.alazmi@outlook.com>
2024-09-15 04:06:37 +0700

    blade parser

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -263,6 +263,7 @@ if true then return {
     opts = {
       ensure_installed = {
         "bash",
+        "blade",
         "html",
         "javascript",
         "json",
@@ -291,7 +292,28 @@ if true then return {
       vim.list_extend(opts.ensure_installed, {
         "tsx",
         "typescript",
+        "blade",
+        "php",
       })
+    end,
+
+    config = function(_, opts)
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+    require("nvim-treesitter.configs").setup(opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
     end,
   },
 
